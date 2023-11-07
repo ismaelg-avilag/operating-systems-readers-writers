d50b118903f00ea073b9669f601acbde16733200
Ismael Avila <ismaelg.avilag@gmail.com>
2023-11-07 15:02:15 -0600

    Clicking on the read button displays the content of the file "content.txt" in the TextArea

--- src/main/java/ReaderWriterWindow.java
+++ src/main/java/ReaderWriterWindow.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.io.*;
+import java.util.ArrayList;
 
 public class ReaderWriterWindow {
     private JPanel mainPanel;
@@ -7,19 +9,46 @@ public class ReaderWriterWindow {
     private JButton writeButton;
     private JTextArea textArea;
 
-    public ReaderWriterWindow() {
+    ArrayList<String> fileContent = new ArrayList<>();
 
+
+    public ReaderWriterWindow() {
         readButton.addActionListener(e -> {
+            textArea.setText("");
+            fileContent.clear();
+
+            try {
+                BufferedReader reader = new BufferedReader(new FileReader("ReaderWriterFiles/content.txt"));
+
+                String line = reader.readLine();
+                while (line != null) {
+                    fileContent.add(line + "\n");
+                    line = reader.readLine();
+                }
+
+                for(String s : fileContent) {
+                    textArea.append(s);
+                }
+
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
         });
+
         writeButton.addActionListener(e -> {
         });
+
         saveButton.addActionListener(e -> {
         });
     }
 
-    public void setVisible(boolean b)
-    {
-        mainPanel.setVisible(b);
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("ReaderWriterWindow");
+        frame.setContentPane(new ReaderWriterWindow().mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 
 }
