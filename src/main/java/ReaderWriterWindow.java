286c06fa4ea18f88829bf7b14946ecbd785a0fb6
Ismael Avila <ismaelg.avilag@gmail.com>
2023-11-07 14:19:52 -0600

    Reader-Writer window created

---
+++ src/main/java/ReaderWriterWindow.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+
+public class ReaderWriterWindow {
+    private JPanel mainPanel;
+    private JButton readButton;
+    private JButton saveButton;
+    private JButton writeButton;
+    private JTextArea textArea;
+
+    public ReaderWriterWindow() {
+
+        readButton.addActionListener(e -> {
+        });
+        writeButton.addActionListener(e -> {
+        });
+        saveButton.addActionListener(e -> {
+        });
+    }
+
+    public void setVisible(boolean b)
+    {
+        mainPanel.setVisible(b);
+    }
+
+}
