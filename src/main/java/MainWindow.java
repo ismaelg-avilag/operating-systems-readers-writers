3904b495c8eb84de81a5ec5679f9c05e751fb43f
Ismael Avila <ismaelg.avilag@gmail.com>
2023-11-09 00:12:23 -0600

    - Write semaphore
    - Added a write semaphore
    - Added a list of "ReaderWriter" windows, to enable or disable them when a window has write permission
    - When one window is writing, the others are disabled, released when writing is canceled or changes are saved.
    - The main method in ReaderWriterWindow is no longer needed

--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -1,27 +1,42 @@
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.concurrent.Semaphore;
 
 public class MainWindow {
     private JButton generateWindowsButton;
     private JPanel mainPanel;
     private JSpinner spinner;
 
+    private ArrayList<ReaderWriterWindow> windowsList = new ArrayList<>();
+    private Semaphore writingSemaphore;
+
     public MainWindow()
     {
         spinner.setModel(new SpinnerNumberModel(2, 2, 5, 1));
 
         generateWindowsButton.addActionListener(e -> {
-            int windows = (int) spinner.getValue();
-            for(int i = 0; i < windows; i++) {
-                JFrame frame = new JFrame("ReaderWriterWindow");
-                frame.setContentPane(new ReaderWriterWindow().mainPanel);
-                frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-                frame.pack();
-                frame.setVisible(true);
-            }
+            createMultipleWindows((int) spinner.getValue());
 
             spinner.setEnabled(false);
             generateWindowsButton.setEnabled(false);
         });
+    }
+
+    private void createMultipleWindows(int windows)
+    {
+        writingSemaphore = new Semaphore(1);
+
+        for(int i = 0; i < windows; i++) {
+            JFrame frame = new JFrame("Lector - Escritor " + (i + 1));
+            ReaderWriterWindow window = new ReaderWriterWindow(writingSemaphore, windowsList);
+
+            frame.setContentPane(window.mainPanel);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.pack();
+            frame.setVisible(true);
+
+            windowsList.add(window);
+        }
     }
 
 
